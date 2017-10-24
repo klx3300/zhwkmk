a3bbdf7197961e51b8d4e10690a59790713eb648
klx3300 <inc19991@gmail.com>
2017-10-24 17:27:41 +0800

    written part gendest

--- gendest/gendest.cpp
+++ gendest/gendest.cpp
@@ -1,1 +1,24 @@
 #include "gendest.h"
+#include "../stroper/stroper.h"
+
+using namespace std;
+
+string gendest_ref(string name){
+    return "$("+name+")";
+}
+
+void gendest_assign(vector<string> &output,vector<FuncParams> call){
+    output.push_back(call[1].str+" := "+call[2].str);
+}
+
+void gendest_eval(vector<string> &output,vector<FuncParams> call){
+    output.push_back(call[1].str+=" := `"+call[2].str+"`");
+}
+
+void gendest_obj(vector<string> &output,vector<FuncParams> call,string dir){
+
+}
+
+void gendest_exe(vector<string> &output,vector<FuncParams> call,string dir){
+
+}
